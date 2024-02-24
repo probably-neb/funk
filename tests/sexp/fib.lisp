e65d4a385353df8c55f752ab8abb1139c26ee1dd
Ben Kunkle <ben.kunkle@gmail.com>
2024-02-24 09:01:15 -0800

    no return in fib.lisp

--- tests/sexp/fib.lisp
+++ tests/sexp/fib.lisp
@@ -1,11 +1,8 @@
-(fun fib (n) (
-    (if (<= n 1)
-        (return n)
-        (return (+
-                  (fib (- n 1))
-                  (fib (- n 2))
-                  ))
-    )
-))
+(fun fib (n)
+     (if (<= n 1)
+       n
+       (+ (fib (- n 1)) (fib (- n 2)))
+       )
+ )
 
-(fib 10)
+(fib 30)
